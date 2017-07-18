a0915ee5a6d904cc91c1c05c69423b044ccbc8b4
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-07-18 18:50:40 +0530

    jquery plugin used for first time which converts numbers to star rating

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,17 @@ class MoviesController < ApplicationController
   end
 
   def show
+    @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
+
+    if @reviews.blank?
+      @avg_review = 0
+    else
+      @avg_review = @movie.reviews.average(:rating).round(2)
+    end
+
+
   end
+
 
   def new
     @movie = current_user.movies.build
